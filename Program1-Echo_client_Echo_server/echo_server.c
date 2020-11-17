d517a6dc2f5c53cbb1d71190dff8b9241fc09a86
Sks2000Sks <52601700+Sks2000Sks@users.noreply.github.com>
2020-11-17 15:32:40 +0530

    Update echo_server.c

--- Program1-Echo_client_Echo_server/echo_server.c
+++ Program1-Echo_client_Echo_server/echo_server.c
@@ -1,29 +1,45 @@
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<fcntl.h>
-#include <arpa/inet.h>
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+  
 
-void str_echo(int connfd)
-{
-    int n;
-    int bufsize = 1024;
-    char *buffer = malloc(bufsize);
-	
-	again: 
-	while((n=recv(connfd, buffer, bufsize, 0))>0)	
-	send(connfd,buffer,n,0);
-   
-	if(n<0)
-       goto again;
-	
-}
-int main()
-{
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    
+
+        bzero(buff, MAX); 
+  
+        // read the message from client and copy it in buffer 
+        read(sockfd, buff, sizeof(buff)); 
+        // print buffer which contains the client contents 
+        printf("From client: %s\t To client : ", buff); 
+        bzero(buff, MAX); 
+        n = 0; 
+        // copy server message in the buffer 
+        while ((buff[n++] = getchar()) != '\n'); 
+  
+        // and send that buffer to client 
+        write(sockfd, buff, sizeof(buff)); 
+  
+        // if msg contains "Exit" then server exit and chat ended. 
+        if (strncmp("exit", buff, 4) == 0) { 
+            printf("Server Exit...\n"); 
+            break; 
+        } 
+
+} 
+
+int main() 
+{ 
     int cont,listenfd,connfd,addrlen,addrlen2,fd,pid,addrlen3;
     
     struct sockaddr_in address,cli_address;
@@ -53,10 +69,10 @@ int main()
 		{
 		  printf("inside child\n");
 		  close(listenfd);
-		  str_echo(connfd);
+		  func(connfd);
 		  exit(0);
 		} 
 		close(connfd);
 	}
     return 0 ;
-}
+} 
